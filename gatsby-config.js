cdc24a0a165fde7c780676468179c21cfb71603b
secretmr <mete.rota.6383@gmail.com>
2019-08-13 17:30:53 +0300

    * logos updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,11 +59,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `IF ve FOR`,
+        short_name: `Geliştiremiyoruz`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ffa500`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
